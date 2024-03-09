fd1eafabce0c57b6fdb5f695df9e3de1c625ff43
Alex Storm <info@alexstorm.ru>
2024-03-09 09:47:27 +0530

    first

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { createSignal, lazy, createEffect, createRoot, createMemo, createComputed, Show, Component } from "solid-js";
+
+export {
+  createSignal as signal,
+  createRoot as root,
+  createMemo as memo,
+  createComputed as computed,
+  lazy,
+  createEffect as effect,
+  Show,
+  Component,
+}
