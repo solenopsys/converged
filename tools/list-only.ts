b91ae26ce54c93b3bf078678e7b2890b81971773
Alex Storm <info@solenopsys.org>
2026-03-06 14:56:25 +0700

    trash scripts removed

--- tools/list-only.ts
+++
@@ -1,5 +0,0 @@
-import { StorageConnection } from "../back/native/bun-transport/src/index";
-const conn = new StorageConnection('/tmp/storage-socket/storage.sock');
-const stores = conn.listStores();
-console.log('LIST_OK', stores.length);
-conn.close();
--- tools/ping-twice.ts
+++
@@ -1,7 +0,0 @@
-import { StorageConnection } from "../back/native/bun-transport/src/index";
-const conn = new StorageConnection('/tmp/storage-socket/storage.sock');
-conn.ping();
-console.log('PING1');
-conn.ping();
-console.log('PING2');
-conn.close();
